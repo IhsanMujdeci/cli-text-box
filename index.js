402b9d8a7edfc5cf7c16f5dbb060e268672b3f24
ihsan <ihsan@driveyello.com>
2019-01-21 13:29:37 +1100

    more examples

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ function consoleTextBox(options, ...textLines){
         console.log(fillLine);
     }
     console.log(bottomBorder);
-
 }
 
 function genLine(text, maxLength){
@@ -97,11 +96,13 @@ function genLine(text, maxLength){
 
 function examples(){
     consoleTextBox('hey', 'dude');
-    consoleTextBox('hey', 'dude', 'im kind of long', 'i have a \n new line', `im a multi
+    consoleTextBox('hey', 'dude', 'im kind of long, can you see?', 'i have a \n new line', `im a multi
     line back tick`);
-    consoleTextBox({sideBuffer: 1, topBottomBuffer: 0}, 'I have options', 'Look at me mum')
-    const myBindTextBox = consoleTextBox.bind({sideBuffer: 3, topBottomBuffer: 2});
-    const myClosureTextBox = (...textLines) => consoleTextBox({sideBuffer: 3, topBottomBuffer: 2}, ...textLines)
+    consoleTextBox({sideBuffer: 1, topBottomBuffer: 0}, 'I have options', 'Look at me mum');
+    const myBoundTextBox = consoleTextBox.bind({sideBuffer: 3, topBottomBuffer: 2});
+    myBoundTextBox('I have options', 'bound 2');
+    const myClosureTextBox = (...textLines) => consoleTextBox({sideBuffer: 3, topBottomBuffer: 2}, ...textLines);
+    myClosureTextBox('I also have options', 'I wont dis-close how i got them')
 }
 
 module.exports = consoleTextBox;
